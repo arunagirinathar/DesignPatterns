938c9002425e57e43744365d2c3e81e5a3a2834e
R. Sivaramakrishna Sharma <arunagirinathar@gmail.com>
2025-04-13 10:57:15 +0530

    Enhance Singleton class with instance control methods
    - Added `initialize()` to allow one-time initialization with arguments.
    - Introduced `forceInitialize()` to forcefully overwrite an existing instance.
    - Implemented `deleteInstance()` to explicitly remove the current instance.
    - Added `resetInstance()` to delete and reinitialize the instance.
    - Prevent serialization and cloning with `__sleep()`, `__clone()`, and `__wakeup()` to maintain single instance integrity.
    - Update version to v1.1.0
    
    Release: v1.1.0

--- src/Singleton.php
+++ src/Singleton.php
@@ -3,11 +3,10 @@
 namespace Arunagirinathar\DesignPatterns;
 
 /**
- * Sinlgeton class
+ * Singleton class
  *
  * This class is used to create a singleton instance of a class
- * This class is abstract and should be extended by the class that needs to be a singleton
- * This class is a part of the design pattern package
+ * This class is abstract and should be extended by the class that needs to be a singleton.
  * php version 8
  *
  * @category DesignPattern
@@ -15,6 +14,8 @@ namespace Arunagirinathar\DesignPatterns;
  * @author   Arunagirinathar <arunagirinathar@gmail.com>
  * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
  * @link     https://github.com/arunagirinathar/DesignPatterns
+ * @since    1.0.0
+ * @version  1.1.0
  */
 abstract class Singleton
 {
@@ -24,20 +25,86 @@ abstract class Singleton
 
   private function __clone() {} // Prevent cloning
   private function __wakeup() {} // Prevent unserializing
-  // private function __sleep() {} // Prevent serializing
+
+  // @since 1.1.0
+  private function __sleep() {} // Prevent serializing
 
   /**
    * Get the singleton instance
-   *
+   *  
    * @return static
+   * @since 1.0.0
    */
   public static function getInstance(): static
   {
     $class = static::class;
 
     if (!isset(self::$_instances[$class])) {
+      // This is left for backwards compatibility
       self::$_instances[$class] = new static();
     }
     return self::$_instances[$class];
+  }
+
+  /**
+   * Initialize a class with arguments (getInstance does not support arguments)
+   * This method does not overwrite an existing instance.
+   *
+   * @param mixed ...$args
+   * @return static
+   * @since 1.1.0
+   */
+  public static function initialize(...$args): static
+  {
+    $class = static::class;
+    if (!isset(self::$_instances[$class])) {
+      self::$_instances[$class] = new static(...$args);
+    } else {
+      trigger_error($class . ' has already been instantiated. Use forceInitialize() to overwrite the existing instance.', E_USER_WARNING);
+    }
+
+    return static::getInstance();
+  }
+
+  /**
+   * Force initialization and overwrite an existing instance.
+   *
+   * @param mixed ...$args
+   * @return static
+   * @since 1.1.0
+   */
+  public static function forceInitialize(...$args): static
+  {
+    $class = static::class;
+    if (isset(self::$_instances[$class])) {
+      trigger_error('Forcibly overwriting the existing instance of ' . $class, E_USER_WARNING);
+    }
+
+    self::$_instances[$class] = new static(...$args);
+
+    return static::getInstance();
+  }
+
+  /**
+   * Delete the existing instance.
+   *
+   * @return void
+   * @since 1.1.0
+   */
+  public static function deleteInstance(): void
+  {
+    $class = static::class;
+    unset(self::$_instances[$class]);
+  }
+
+  /**
+   * Reset and reinitialize the instance.
+   *
+   * @return static
+   */
+  public static function resetInstance(...$args): static
+  {
+    self::deleteInstance();
+    return self::initialize(...$args); // Pass args if needed for reinitialization
   }
 }
