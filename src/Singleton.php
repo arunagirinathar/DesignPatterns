7392f777ad09d341f6ceae3ad0e75035148a87ee
R. Sivaramakrishna Sharma <arunagirinathar@gmail.com>
2025-04-13 18:35:20 +0530

    Refactor Singleton class: - Improved exception messages in __wakeup() and __sleep() methods for better clarity. - Added and refined docblocks for clarity and consistency across methods. - Minor code cleanups for better maintainability and readability.

--- src/Singleton.php
+++ src/Singleton.php
@@ -15,100 +15,124 @@ namespace Arunagirinathar\DesignPatterns;
  * @license  GNU GPLv3 https://www.gnu.org/licenses/gpl-3.0.en.html
  * @link     https://github.com/arunagirinathar/DesignPatterns
  * @since    1.0.0
- * @version  1.1.1
+ * @version  1.2.2
  */
 abstract class Singleton
 {
-  private static array $_instances = [];
+    private static array $_instances = [];
 
-  protected function __construct() {}
+    /**
+     * Singleton constructor is private to prevent direct instantiation.
+     */
+    protected function __construct() {}
 
-  private function __clone() {} // Prevent cloning
-  private function __wakeup() {} // Prevent unserializing
-
-  // @since 1.1.0
-  public function __sleep() {
-
-    // Fixed to return an empty array to prevent serialization
-    return [];
-  } // Prevent serializing
-
-  /**
-   * Get the singleton instance
-   *  
-   * @return static
-   * @since 1.0.0
-   */
-  public static function getInstance(): static
-  {
-    $class = static::class;
-
-    if (!isset(self::$_instances[$class])) {
-      // This is left for backwards compatibility
-      self::$_instances[$class] = new static();
-    }
-    return self::$_instances[$class];
-  }
-
-  /**
-   * Initialize a class with arguments (getInstance does not support arguments)
-   * This method does not overwrite an existing instance.
-   *
-   * @param mixed ...$args
-   * @return static
-   * @since 1.1.0
-   */
-  public static function initialize(...$args): static
-  {
-    $class = static::class;
-    if (!isset(self::$_instances[$class])) {
-      self::$_instances[$class] = new static(...$args);
-    } else {
-      trigger_error($class . ' has already been instantiated. Use forceInitialize() to overwrite the existing instance.', E_USER_WARNING);
+    /**
+     * Prevent Cloning of Singleton instances.
+     *
+     * @throws \Exception
+     */
+    public function __clone()
+    {
+        throw new \Exception("Cloning is not allowed on this Singleton object.");
     }
 
-    return static::getInstance();
-  }
-
-  /**
-   * Force initialization and overwrite an existing instance.
-   *
-   * @param mixed ...$args
-   * @return static
-   * @since 1.1.0
-   */
-  public static function forceInitialize(...$args): static
-  {
-    $class = static::class;
-    if (isset(self::$_instances[$class])) {
-      trigger_error('Forcibly overwriting the existing instance of ' . $class, E_USER_WARNING);
+    /**
+     * Prevent Serialization of Singleton instances.
+     *
+     * @throws \Exception
+     */
+    public function __sleep()
+    {
+        throw new \Exception("Serialization is not allowed for this Singleton object.");
     }
 
-    self::$_instances[$class] = new static(...$args);
+    /**
+     * Prevent Unserialization of Singleton instances.
+     *
+     * @throws \Exception
+     */
+    public function __wakeup()
+    {
+        throw new \Exception("Unserialization is not allowed for this Singleton object.");
+    }
 
-    return static::getInstance();
-  }
+    /**
+     * Get the Singleton instance.
+     * 
+     * @return static
+     * @since 1.0.0
+     */
+    public static function getInstance(): static
+    {
+        $class = static::class;
 
-  /**
-   * Delete the existing instance.
-   *
-   * @return void
-   * @since 1.1.0
-   */
-  public static function deleteInstance(): void
-  {
-    $class = static::class;
-    unset(self::$_instances[$class]);
-  }
+        if (!isset(self::$_instances[$class])) {
+            self::$_instances[$class] = new static();
+        }
+        return self::$_instances[$class];
+    }
 
-  /**
-   * Reset and reinitialize the instance.
-   *
-   * @return static
-   */
-  public static function resetInstance(...$args): static
-  {
-    self::deleteInstance();
-    return self::initialize(...$args); // Pass args if needed for reinitialization
-  }
+    /**
+     * Initialize a class with arguments (getInstance does not support arguments).
+     * This method does not overwrite an existing instance.
+     *
+     * @param mixed ...$args
+     * @return static
+     * @since 1.1.0
+     */
+    public static function initialize(...$args): static
+    {
+        $class = static::class;
+        if (!isset(self::$_instances[$class])) {
+            self::$_instances[$class] = new static(...$args);
+        } else {
+            trigger_error($class . ' has already been instantiated. Use forceInitialize() to overwrite the existing instance.', E_USER_WARNING);
+        }
+
+        return static::getInstance();
+    }
+
+    /**
+     * Force initialization and overwrite an existing instance.
+     *
+     * @param mixed ...$args
+     * @return static
+     * @since 1.1.0
+     */
+    public static function forceInitialize(...$args): static
+    {
+        $class = static::class;
+        if (isset(self::$_instances[$class])) {
+            trigger_error('Forcibly overwriting the existing instance of ' . $class, E_USER_WARNING);
+        }
+
+        self::$_instances[$class] = new static(...$args);
+
+        return static::getInstance();
+    }
+
+    /**
+     * Delete the existing instance.
+     *
+     * @return void
+     * @since 1.1.0
+     */
+    public static function deleteInstance(): void
+    {
+        $class = static::class;
+        unset(self::$_instances[$class]);
+    }
+
+    /**
+     * Reset and reinitialize the instance.
+     *
+     * @param mixed ...$args
+     * @return static
+     * @since 1.1.0
+     */
+    public static function resetInstance(...$args): static
+    {
+        self::deleteInstance();
+        return self::initialize(...$args); // Pass args if needed for reinitialization
+    }
 }
